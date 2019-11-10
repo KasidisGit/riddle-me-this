737020d0697494caf5270f6fd2fb212f24fc9ca5
SukritaEarn <sukrita.ki@ku.th>
2019-11-10 16:54:22 +0700

    fix

--- game/admin.py
+++ game/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HardQuestion, MediumQuestion, EasyQuestion
+from .models import *
 
 admin.site.register(HardQuestion)
 admin.site.register(MediumQuestion)
--- game/views.py
+++ game/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.utils import timezone
-from .models import Question
+from .models import *
 
 def IndexView(request):
     # q = Question.objects.get(pk=1)
