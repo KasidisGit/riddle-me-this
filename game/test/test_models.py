3612fdaae1a695149e824a9404be7befbfa1ec36
noeybynk <boonyanuch.noey2000@gmail.com>
2019-11-19 21:39:06 +0700

    complete test

--- game/test/test_models.py
+++ game/test/test_models.py
@@ -16,36 +16,68 @@ class QuestionModelTests(TestCase):
         MediumQuestion.objects.create(answer="software")
         HardQuestion.objects.create(answer="tulip")
         HardQuestion.objects.create(answer="championship")
-        EasyQuestion.objects.get(id=2)
-        MediumQuestion.objects.get(id=1)
-        HardQuestion.objects.get(id=1)
-        EasyQuestion.objects.filter(id=1)
-        MediumQuestion.objects.filter(id=2)
-        HardQuestion.objects.filter(id=2)
 
         # for i in MediumQuestion.objects.all():
         #     print(i.id)
 
-    def test_query_for_question(self):
+    def test_query_for_easy_question(self):
         easy1_test = EasyQuestion.objects.get(id=1)
         self.assertEqual(easy1_test.answer,"rainbow")
         easy2_test = EasyQuestion.objects.get(answer="facebook")
         self.assertEqual(easy2_test.id, 2)
+
+    def test_query_for_medium_question(self):
         medium_test = MediumQuestion.objects.filter(answer="software")
         self.assertEqual(medium_test[0].id,2)
         self.assertEqual(medium_test[1].id,3)
+    
+    def test_query_for_hard_question(self):
         hard_test = HardQuestion.objects.get(id=2)
         self.assertEqual(hard_test.answer, "championship")
 
-    def test_less_than_max_length(self):
-        pass
-        
-#     def test_equal_max_length(self):
-#         pass
+    def test_for_add_latest_in_easy(self):
+        easy_test = EasyQuestion.objects.create(answer="pillow")
+        self.assertEqual(len(EasyQuestion.objects.all()), 3)
+        self.assertEqual(EasyQuestion.objects.latest('id').answer, 'pillow')
 
-    def test_more_than_max_length(self):
-        hard_test = HardQuestion.objects.all()
-        self.assertnotEqual(len(hard_test), 300)
+    def test_a_for_delete_in_easy(self):
+        EasyQuestion.objects.get(answer="rainbow").delete()
+        self.assertEqual(len(EasyQuestion.objects.all()),1)
+
+    def test_for_insert_in_easy(self):
+        easy1_test = EasyQuestion(id=20,answer="watermelon")
+        easy2_test = EasyQuestion(id=15,answer="carpet")
+        easy1_test.save()
+        easy2_test.save()
+        self.assertEqual(len(EasyQuestion.objects.all()),4)
+
+    def test_for_add_latest_in_medium(self):
+        medium_test = MediumQuestion.objects.create(id=10, answer="billion")
+        self.assertEqual(len(MediumQuestion.objects.all()), 4)
+        self.assertEqual(MediumQuestion.objects.latest('id').answer, 'billion')
+    
+    def test_a_for_delete_in_medium(self):
+        MediumQuestion.objects.filter(id=2).delete()
+        self.assertEqual(len(MediumQuestion.objects.all()),2)
+    
+    def test_for_insert_in_medium(self):
+        medium_test = MediumQuestion(id=5,answer="peasant")
+        medium_test.save()
+        self.assertEqual(len(MediumQuestion.objects.all()),4)
+
+    def test_for_add_latest_in_hard(self):
+        hard_test = HardQuestion.objects.create(answer="summary")
+        self.assertEqual(len(HardQuestion.objects.all()), 3)
+        self.assertEqual(HardQuestion.objects.latest('id').answer, 'summary')
+    
+    def test_a_for_delete_in_hard(self):
+        HardQuestion.objects.get(id=1).delete()
+        self.assertEqual(len(HardQuestion.objects.all()),1)
+    
+    def test_for_insert_in_hard(self):
+        hard_test = HardQuestion(id=4,answer="sixty")
+        hard_test.save()
+        self.assertEqual(len(HardQuestion.objects.all()),3)
 
     @classmethod
     def tearDownClass(cls):
