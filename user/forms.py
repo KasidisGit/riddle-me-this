51e08e0146a603b2d4d91d351b606b8a4aa5d67f
KasidisGit <kasidis02@gmail.com>
2019-11-21 17:38:17 +0700

    add widget, ignore, require

--- user/forms.py
+++ user/forms.py
@@ -3,8 +3,14 @@ from .models import NameUser
 
 
 class UserForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update(size='20', placeholder='Your name here')
+        self.fields['name'].label = ""
+
     class Meta:
         model = NameUser
-        fields = [
-            'name',
-        ]
+        fields = ['name']
+
+    
