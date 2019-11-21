430e1bbeb45e94bc282a5a1364a1153e4bcea8fb
KasidisGit <kasidis02@gmail.com>
2019-11-21 21:49:00 +0700

    update settings

--- riddle-me-this/settings.py
+++ riddle-me-this/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lh($e$z5lsknsktvf7@v4#0-3ior_l7#o@w087^z8u&=qfpr%f'
+# SECRET_KEY = 'lh($e$z5lsknsktvf7@v4#0-3ior_l7#o@w087^z8u&=qfpr%f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['riddle-me-this-demo.herokuapp.com']
 
 
 # Application definition
