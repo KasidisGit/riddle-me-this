acd4a735fb432fa939fce8ba4aca97f43010bd33
noeybynk <boonyanuch.noey2000@gmail.com>
2019-12-15 23:17:52 +0700

    add complete game page in medium and easy

--- game/views.py
+++ game/views.py
@@ -102,64 +102,23 @@ def HardPicture(request, question_id):
         "is_pass": status,
     })
 
-# def MediumPicture(request,question_id):
-#     user = request.user
-#     Question = MediumQuestion.objects.get(pk=question_id)
-#     if question_id < 20:
-#         next_q = MediumQuestion.objects.get(pk=question_id+1)
-#     else:
-#         next_q = MediumQuestion.objects.get(pk=1)
-
-#     if request.GET.get('hint-btn')== 'Hint':
-#         if user.all_score >= 2:
-#             user.all_score -= 2
-#         else:
-#             pass
-#         user.save()
-
-#     return render(request,"medium_page.html",{
-#         "question": Question,
-#         "question_id" : next_q,
-#         'n' : range(len(Question.answer))
-#     })
-
-# def EasyPicture(request,question_id):
-#     user = request.user
-#     Question = EasyQuestion.objects.get(pk=question_id)
-#     if question_id < 15:
-#         next_q = EasyQuestion.objects.get(pk=question_id+1)
-#     else:
-#         next_q = EasyQuestion.objects.get(pk=1)
-#     # print( request.POST.get('hint-btn'))
-#     if request.GET.get('hint-btn')== 'Hint':
-#         if user.all_score >= 2:
-#             user.all_score -= 2
-#         else:
-#             pass
-#         user.save()
-
-#     return render(request,"easy_page.html",{
-#         "question": Question,
-#         "question_id" : next_q,
-#         'n' : range(len(Question.answer))
-#     })
-
-# def hintforHard(request, question_id):
-#     question = HardQuestion.objects.get(pk=question_id)
-#     ans_q = question.answer
-#     if request.method == 'POST':
-#         for i in ans_q:
-#             list_ans.append(i)
-    
-#     return render(request, "hard_page.html", {
-#         "question_ans" : list_ans,
-#     })
-    
-
 def MediumPicture(request,question_id):
     question = MediumQuestion.objects.get(pk=question_id)
     user = request.user
     status = None
+    key_list = [question.answer]
+    hint_list = []
+    listToStr = []
+    first_key = ' '
+    hintword = question.answer
+
+    if request.POST.get('hint-btn') == 'Hint':
+        if user.all_score >= 2:
+            user.all_score -= 2
+        else:
+            pass
+        user.save()
+
     if request.method == 'POST':
         status = False
         if request.POST.get('textfield',None) == question.answer or request.POST.get('button') == question.answer:
@@ -176,7 +135,7 @@ def MediumPicture(request,question_id):
         next_question = MediumQuestion.objects.get(pk=question_id+1)
     else:
         next_question = MediumQuestion.objects.get(pk=1)
-    return render(request,"medium-question.html",{
+    return render(request,"medium-page.html",{
         "question": question,
         "next_question": next_question,
         "last_question": MediumQuestion.objects.last(),
@@ -204,7 +163,7 @@ def EasyPicture(request,question_id):
         next_question = EasyQuestion.objects.get(pk=question_id+1)
     else:
         next_question = EasyQuestion.objects.get(pk=1)
-    return render(request,"easy-question.html",{
+    return render(request,"easy-page.html",{
         "question": question,
         "next_question": next_question,
         "last_question": EasyQuestion.objects.last(),
